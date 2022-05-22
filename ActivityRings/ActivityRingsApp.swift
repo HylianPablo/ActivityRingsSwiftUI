9ec127b3f799607288014a956311abdad9099a6b
Pablo Martínez López <pmartinezl@full-on-net.com>
2022-05-22 19:46:32 +0200

    Initial Commit

---
+++ ActivityRings/ActivityRingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityRingsApp.swift
+//  ActivityRings
+//
+//  Created by Pablo Martinez Lopez on 22/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ActivityRingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
